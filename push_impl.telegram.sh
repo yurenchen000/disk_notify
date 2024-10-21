9b198e5f3bc4b74ad9ef72dc555196cc71d516a0
chen <yurenchen@yeah.net>
2024-10-22 01:57:52 +0800

    A push_impl.telegram.sh: add push backend telegram

---
+++ push_impl.telegram.sh
@@ -0,0 +1,20 @@
+
+## --------- push via serverChan
+
+## need conf
+# TELEGRAM_BOT_TOKEN=
+# TELEGRAM_CHAT_ID=
+
+
+do_push_telegram(){
+    local tit="$1"
+    local msg="$2"
+
+    curl -s "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+     -d chat_id="$TELEGRAM_CHAT_ID" \
+     -d text="== $tit =="$'\n'"$msg"
+
+	echo 
+    # echo "==`date -Is`: $tit: $msg"  >&2
+}
+
