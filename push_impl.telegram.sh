f783b3d790ab764ff853a0f2e07a13962899481d
chen <yurenchen@yeah.net>
2024-10-25 22:06:46 +0800

    U push_impl.telegram.sh: improve backend telegram: use markdown

--- push_impl.telegram.sh
+++ push_impl.telegram.sh
@@ -5,16 +5,24 @@
 # TELEGRAM_BOT_TOKEN=
 # TELEGRAM_CHAT_ID=
 
-## msg is text //also support markdown,markdownv2,html
+## msg is markdown //also support markdown,markdownv2,html  or text if omit
 do_push_telegram(){
     local tit="$1"
     local msg="$2"
 
+    # msg=
     curl -s "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
      -d chat_id="$TELEGRAM_CHAT_ID" \
-     -d text="== $tit =="$'\n'"$msg"
+     -d text="*== $tit ==*"$'\n'"$msg" \
+     -d parse_mode=markdown
 
 	echo 
     # echo "==`date -Is`: $tit: $msg"  >&2
 }
+
+## omit is text
+## markdown is easy
+## markdownv2 need more escape (by preceding \) : _ * [] () {} ~ ` > # +-= |  . ! 
+## https://core.telegram.org/bots/api#formatting-options
+
 
