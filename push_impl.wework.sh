f695876381c272f116fa694da37fdc470640ca1d
chen <yurenchen@yeah.net>
2024-10-26 13:18:19 +0800

    A push_impl.wework.sh: add backend wework impl

---
+++ push_impl.wework.sh
@@ -0,0 +1,32 @@
+
+## --------- push via feishu bot
+
+## need conf
+# WEWORK_BOT_KEY=
+
+## msg is markdown //also support text (via text.content)
+do_push_wework(){
+    local tit="$1"
+    local msg="$2"
+
+	curl -s "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=$WEWORK_BOT_KEY" \
+    -H "Content-Type: application/json" \
+    -d @- <<-EOF
+	{
+	  "msgtype": "markdown",
+	  "markdown": {
+	    "title": "$tit",
+	    "text": "**== $tit ==**  \n$msg"
+	  }
+	}
+	EOF
+
+	echo 
+    # echo "==`date -Is`: $tit: $msg"  >&2
+}
+
+## note: this file indent need to be tabs, cause bash here-doc
+## wework can't send to wechat
+## msg formats: https://developer.work.weixin.qq.com/document/path/99110#markdown%E7%B1%BB%E5%9E%8B
+
+
