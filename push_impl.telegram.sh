043be786221d03e5e2ace50d8ea4f5d71a374369
chen <yurenchen@yeah.net>
2025-09-07 15:44:43 +0800

    U push_impl.telegram.sh: add TELEGRAM_PROXY for https_proxy curl telegram api

--- push_impl.telegram.sh
+++ push_impl.telegram.sh
@@ -4,6 +4,7 @@
 ## need conf
 # TELEGRAM_BOT_TOKEN=
 # TELEGRAM_CHAT_ID=
+# TELEGRAM_PROXY=  ## use http/socks proxy
 
 ## msg is markdown //also support markdown,markdownv2,html  or text if omit
 do_push_telegram(){
@@ -11,7 +12,7 @@ do_push_telegram(){
     local msg="$2"
 
     # msg=
-    curl -s "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+    https_proxy="$TELEGRAM_PROXY" curl -s "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
      -d chat_id="$TELEGRAM_CHAT_ID" \
      -d text="*== $tit ==*"$'\n'"$msg" \
      -d parse_mode=markdown
